54470a40e698807e1c756b1a74acf94238989004
ping yang <yangping@idea.edu.cn>
2022-01-19 15:28:55 +0800

    add clue task with classification

---
+++ fengshen/examples/finetune_classification.py
@@ -0,0 +1,145 @@
+from transformers import TrainingArguments,Trainer,BertModel,AutoTokenizer,AutoModelForSequenceClassification
+import torch 
+import json
+from tqdm import tqdm
+import os
+import numpy as np
+from transformers import EvalPrediction
+from label_desc import task2label
+import sys
+from transformers import (
+    TrainingArguments,
+    HfArgumentParser,
+)
+
+
+os.environ["CUDA_VISIBLE_DEVICES"] = '7'
+
+
+def load_schema(task_name):
+    label2id,id2label={},{}
+    for i,k in enumerate(task2label[task_name].keys()):
+        label2id[k]=i
+        id2label[i]=k
+    return label2id,id2label
+
+
+def load_data(file_path,task_name,label2id):
+    with open(file_path, 'r', encoding='utf8') as f:
+        lines = f.readlines()
+        result=[]
+        for line in tqdm(lines):
+            data = json.loads(line)
+            text_id = int(data['id']) if 'id' in data.keys() else 0  #在该份数据集中只有测试集才有id字段
+            texta,textb,labels='','',0
+            if task_name=='afqmc':
+                texta = data['sentence1']
+                textb = data['sentence2']
+                labels = label2id[data['label']] if 'label' in data.keys() else 0 #测试集中没有label标签，默认为0
+            elif task_name=='tnews':
+                texta = data['sentence']
+                labels = label2id[data['label_desc']] if 'label_desc' in data.keys() else 0 #测试集中没有label标签，默认为0
+            elif task_name=='iflytek':
+                texta = data['sentence']
+                labels = label2id[data['label_des']] if 'label_des' in data.keys() else 0 #测试集中没有label标签，默认为0
+            elif task_name=='ocnli':
+                texta = data['sentence1']
+                textb = data['sentence2']
+                labels = label2id[data['label']] if 'label' in data.keys() else 0 #测试集中没有label标签，默认为0
+            elif task_name=='wsc':
+                target = data['target']
+                text=list(data['text'])
+                if target['span2_index']<target['span1_index']:
+                    text.insert(target['span2_index'],'_')
+                    text.insert(target['span2_index']+len(target['span2_text'])+1,'_')
+                    text.insert(target['span1_index']+2,'[')
+                    text.insert(target['span1_index']+2+len(target['span1_text'])+1,']')
+                else:
+                    text.insert(target['span1_index'],'[')
+                    text.insert(target['span1_index']+len(target['span1_text'])+1,']')
+                    text.insert(target['span2_index']+2,'_')
+                    text.insert(target['span2_index']+2+len(target['span2_text'])+1,'_')
+                texta=''.join(text)
+                labels = label2id[data['label']] if 'label' in data.keys() else 0 #测试集中没有label标签，默认为0
+            elif task_name=='csl':
+                texta = '； '.join(data['keyword'])
+                textb = data['abst']
+                labels = label2id[data['label']] if 'label' in data.keys() else 0 #测试集
+            result.append({'texta':texta,'textb':textb,'labels':labels,'id':text_id})
+        print(result[0])
+        return result
+
+
+class taskDataset(torch.utils.data.Dataset):
+    def __init__(self, data,tokenizer,max_length):
+        self.data = data
+        self.tokenizer = tokenizer
+        self.max_length = max_length
+
+    def __len__(self):
+        return len(self.data)
+
+    def __getitem__(self, index):
+        item = self.data[index]
+        if 'textb' in item.keys() and item['textb'] != '':
+            encode_dict = self.tokenizer.encode_plus(item['texta'],item['textb'],
+                                                    max_length=self.max_length,
+                                                    padding='max_length',
+                                                    truncation='longest_first')
+        else:
+            encode_dict = self.tokenizer.encode_plus(item['texta'],  #
+                                                    max_length=self.max_length,
+                                                    padding='max_length',
+                                                    truncation='longest_first')
+
+        return {
+            "input_ids": torch.tensor(encode_dict['input_ids']).long(),
+            "token_type_ids": torch.tensor(encode_dict['token_type_ids']).long(),
+            "attention_mask": torch.tensor(encode_dict['attention_mask']).float(),
+            "labels": torch.tensor(item['labels']).long()
+            }
+ 
+
+def compute_metrics(p: EvalPrediction):
+    preds = p.predictions[0] if isinstance(p.predictions, tuple) else p.predictions
+    preds = np.argmax(preds, axis=1)
+    return {"accuracy": (preds == p.label_ids).astype(np.float32).mean().item()}
+
+
+def main():
+    parser = HfArgumentParser(TrainingArguments)
+    parser.add_argument("--task_name", type=str, help="test file")
+    parser.add_argument("--train_data_path", type=str, help="train file")
+    parser.add_argument("--dev_data_path", type=str, help="dev file")
+    parser.add_argument("--test_data_path", type=str, help="test file")
+    parser.add_argument("--pretrained_model_path", type=str, help="pretrained_model_path")
+    parser.add_argument("--max_length", type=int, help="max_length")
+
+    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
+        training_args,task_args = parser.parse_json_file(
+            json_file=os.path.abspath(sys.argv[1]))
+    else:
+        training_args,task_args = parser.parse_args_into_dataclasses()
+
+    label2id,_=load_schema(task_args.task_name)
+    model = AutoModelForSequenceClassification.from_pretrained(task_args.pretrained_model_path, num_labels=len(label2id))
+    tokenizer = AutoTokenizer.from_pretrained(task_args.pretrained_model_path)
+
+    train_data = load_data(task_args.train_data_path,task_args.task_name,label2id)
+    dev_data = load_data(task_args.dev_data_path,task_args.task_name,label2id)
+    train_dataset = taskDataset(train_data,tokenizer=tokenizer,max_length=task_args.max_length)
+    dev_dataset = taskDataset(dev_data,tokenizer=tokenizer,max_length=task_args.max_length)
+
+    trainer = Trainer(
+        model=model,                         # the instantiated   Transformers model to be trained
+        args=training_args,                  # training arguments, defined above
+        train_dataset=train_dataset,         # training dataset
+        eval_dataset=dev_dataset,            # evaluation datase
+        compute_metrics=compute_metrics
+    )
+
+    trainer.train()
+    
+
+if __name__=="__main__":
+    main()
---
+++ fengshen/scripts/finetune_classification.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+#SBATCH --job-name=slurm-test # create a short name for your job
+#SBATCH --nodes=1 # node count
+#SBATCH --ntasks=1 # total number of tasks across all nodes
+#SBATCH --cpus-per-task=2 # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --mem-per-cpu=16G # memory per cpu-core (4G is default)
+#SBATCH --gres=gpu:1 # number of gpus per node
+#SBATCH --mail-type=ALL # send email when job begins, ends or failed etc. 
+
+BERT_NAME=macbert_base
+ROOT_PATH=cognitive_comp
+TASK=csl
+
+TRAIN_DATA_PATH=/$ROOT_PATH/yangping/data/ChineseCLUE_DATA/${TASK}_public/train.json
+DEV_DATA_PATH=/$ROOT_PATH/yangping/data/ChineseCLUE_DATA/${TASK}_public/dev.json
+TSET_DATA_PATH=/$ROOT_PATH/yangping/data/ChineseCLUE_DATA/${TASK}_public/test.json
+
+
+PRETRAINED_MODEL_PATH=/$ROOT_PATH/yangping/pretrained_model/$BERT_NAME/
+
+
+CHECKPOINT_PATH=/$ROOT_PATH/yangping/checkpoints/modelevaluation/csl/
+OUTPUT_PATH=/$ROOT_PATH/yangping/nlp/modelevaluation/output/csl_predict.json
+
+LOG_FILE_PATH=/$ROOT_PATH/yangping/nlp/modelevaluation/log/
+
+options=" \
+        --task_name $TASK \
+        --train_data_path $TRAIN_DATA_PATH \
+        --dev_data_path $DEV_DATA_PATH \
+        --test_data_path $TSET_DATA_PATH \
+        --pretrained_model_path $PRETRAINED_MODEL_PATH \
+        --max_length 128 \
+        --output_dir $LOG_FILE_PATH \
+        --num_train_epochs 3 \
+        --per_device_train_batch_size 32 \
+        --per_device_eval_batch_size 128 \
+        --learning_rate 0.00002 \
+        --warmup_steps 1000 \
+        --weight_decay 0.01 \
+        --max_grad_norm 0.1 \
+        --logging_dir $LOG_FILE_PATH \
+        --logging_steps 10 \
+        --do_eval true \
+        --evaluation_strategy steps \
+        --save_steps 100 \
+        --eval_steps 100 \
+        --save_total_limit 3 \
+        --load_best_model_at_end true \
+        --metric_for_best_model eval_accuracy \
+        "
+
+DOCKER_PATH=/$ROOT_PATH/yangping/containers/pytorch21_06_py3_docker_image.sif
+SCRIPT_PATH=/$ROOT_PATH/yangping/nlp/Fengshenbang-LM/fengshen/examples/finetune_classification.py
+
+python3 $SCRIPT_PATH $options
+# singularity exec --nv -B /cognitive_comp/:/cognitive_comp/ $DOCKER_PATH python3 $SCRIPT_PATH $options
+
