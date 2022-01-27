2e4ff244ba94f5aef0c197f7823fc351645b72b5
ping yang <yangping@idea.edu.cn>
2022-01-27 16:52:04 +0800

    refactor hf to pl

--- fengshen/examples/finetune_classification.py
+++ fengshen/examples/finetune_classification.py
@@ -13,107 +13,75 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+from logging import basicConfig
 import torch
+from torch import nn
 import json
 from tqdm import tqdm
+from typing import Optional
 import os
 import numpy as np
 from transformers import EvalPrediction
+from transformers.utils.dummy_pt_objects import RemBertForSequenceClassification
 from label_desc import task2label
 import sys
 from transformers import (
-    TrainingArguments,
-    HfArgumentParser,
-    Trainer,
     AutoTokenizer,
-    # AutoModelForSequenceClassification
+    AutoModelForSequenceClassification
 )
+import pytorch_lightning as pl
 
-from auto.modeling_auto import AutoModelForSequenceClassification
-
+from pytorch_lightning.callbacks import ModelCheckpoint
+from pytorch_lightning import Trainer
+from torch.utils.data import Dataset, DataLoader
+from transformers.optimization import get_linear_schedule_with_warmup
+# from fengshen import AutoModelForSequenceClassification
+# from fengshen import AutoTokenizer
+import argparse
 os.environ["CUDA_VISIBLE_DEVICES"] = '7'
 
-def load_schema(task_name):
-    label2id, id2label = {}, {}
-    for i, k in enumerate(task2label[task_name].keys()):
-        label2id[k] = i
-        id2label[i] = k
-    return label2id, id2label
 
-def load_data(file_path, task_name, label2id):
-    with open(file_path, 'r', encoding='utf8') as f:
-        lines = f.readlines()
-        result = []
-        for line in tqdm(lines):
-            data = json.loads(line)
-            text_id = int(data['id']) if 'id' in data.keys() else 0
-            texta, textb, labels = '', '', 0
-            if task_name == 'afqmc':
-                texta = data['sentence1']
-                textb = data['sentence2']
-                labels = label2id[data['label']
-                                  ] if 'label' in data.keys() else 0
-            elif task_name == 'tnews':
-                texta = data['sentence']
-                labels = label2id[data['label_desc']
-                                  ] if 'label_desc' in data.keys() else 0
-            elif task_name == 'iflytek':
-                texta = data['sentence']
-                labels = label2id[data['label_des']
-                                  ] if 'label_des' in data.keys() else 0
-            elif task_name == 'ocnli':
-                texta = data['sentence1']
-                textb = data['sentence2']
-                labels = label2id[data['label']
-                                  ] if 'label' in data.keys() else 0
-            elif task_name == 'wsc':
-                target = data['target']
-                text = list(data['text'])
-                if target['span2_index'] < target['span1_index']:
-                    text.insert(target['span2_index'], '_')
-                    text.insert(target['span2_index'] +
-                                len(target['span2_text'])+1, '_')
-                    text.insert(target['span1_index']+2, '[')
-                    text.insert(target['span1_index']+2 +
-                                len(target['span1_text'])+1, ']')
-                else:
-                    text.insert(target['span1_index'], '[')
-                    text.insert(target['span1_index'] +
-                                len(target['span1_text'])+1, ']')
-                    text.insert(target['span2_index']+2, '_')
-                    text.insert(target['span2_index']+2 +
-                                len(target['span2_text'])+1, '_')
-                texta = ''.join(text)
-                labels = label2id[data['label']
-                                  ] if 'label' in data.keys() else 0
-            elif task_name == 'csl':
-                texta = '； '.join(data['keyword'])
-                textb = data['abst']
-                labels = label2id[data['label']
-                                  ] if 'label' in data.keys() else 0  
-            result.append({'texta': texta, 'textb': textb,
-                          'labels': labels, 'id': text_id})
-        print(result[0])
-        return result
-
-class taskDataset(torch.utils.data.Dataset):
-    def __init__(self, data, tokenizer, max_length):
-        self.data = data
-        self.tokenizer = tokenizer
-        self.max_length = max_length
+class TaskDataset(Dataset):
+    def __init__(self, data_path, args, label2id):
+        super().__init__()
+        self.tokenizer = AutoTokenizer.from_pretrained(
+            args.pretrained_model_path)
+        self.label2id = label2id
+        self.max_length = args.max_length
+        self.data = self.load_data(data_path, args)
 
     def __len__(self):
         return len(self.data)
 
     def __getitem__(self, index):
-        item = self.data[index]
-        if 'textb' in item.keys() and item['textb'] != '':
-            encode_dict = self.tokenizer.encode_plus(item['texta'], item['textb'],
+        return self.encode(self.data[index])
+
+    def load_data(self, data_path, args):
+        with open(data_path, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+            samples = []
+            for line in tqdm(lines):
+                data = json.loads(line)
+                text_id = int(data[args.id_name]
+                              ) if args.id_name in data.keys() else 0
+                texta = data[args.texta_name] if args.texta_name in data.keys(
+                ) else ''
+                textb = data[args.textb_name] if args.textb_name in data.keys(
+                ) else ''
+                labels = self.label2id[data[args.label_name]
+                                       ] if args.label_name in data.keys() else 0
+                samples.append({'texta': texta, 'textb': textb,
+                                'labels': labels, 'id': text_id})
+        return samples
+
+    def encode(self, item):
+        if item['texta'] != '' and item['textb'] != '':
+            encode_dict = self.tokenizer.encode_plus([item['texta'], item['textb']],
                                                      max_length=self.max_length,
                                                      padding='max_length',
                                                      truncation='longest_first')
         else:
-            encode_dict = self.tokenizer.encode_plus(item['texta'],  #
+            encode_dict = self.tokenizer.encode_plus(item['texta'],
                                                      max_length=self.max_length,
                                                      padding='max_length',
                                                      truncation='longest_first')
@@ -122,56 +90,221 @@ class taskDataset(torch.utils.data.Dataset):
             "input_ids": torch.tensor(encode_dict['input_ids']).long(),
             "token_type_ids": torch.tensor(encode_dict['token_type_ids']).long(),
             "attention_mask": torch.tensor(encode_dict['attention_mask']).float(),
-            "labels": torch.tensor(item['labels']).long()
+            "labels": torch.tensor(item['labels']).long(),
         }
 
 
-def compute_metrics(p: EvalPrediction):
-    preds = p.predictions[0] if isinstance(
-        p.predictions, tuple) else p.predictions
-    preds = np.argmax(preds, axis=1)
-    return {"accuracy": (preds == p.label_ids).astype(np.float32).mean().item()}
+class TaskDataModel(pl.LightningDataModule):
+    @staticmethod
+    def add_data_specific_args(parent_args):
+        parser = parent_args.add_argument_group('TASK NAME DataModel')
+        parser.add_argument('--data_dir', default='./data', type=str)
+        parser.add_argument('--num_workers', default=8, type=int)
+        parser.add_argument('--train_data', default='train.json', type=str)
+        parser.add_argument('--valid_data', default='dev.json', type=str)
+        parser.add_argument('--test_data', default='test.json', type=str)
+        parser.add_argument('--train_batchsize', default=16, type=int)
+        parser.add_argument('--valid_batchsize', default=32, type=int)
+        parser.add_argument('--max_length', default=128, type=int)
+
+        parser.add_argument('--texta_name', default='text', type=str)
+        parser.add_argument('--textb_name', default='sentence2', type=str)
+        parser.add_argument('--label_name', default='label', type=str)
+        parser.add_argument('--id_name', default='id', type=str)
+
+        return parent_args
+
+    def __init__(self, args):
+        super().__init__()
+        self.train_batchsize = args.train_batchsize
+        self.valid_batchsize = args.valid_batchsize
+        self.label2id, self.id2label = self.load_schema(os.path.join(
+            args.data_dir, args.train_data), args)
+        self.train_data = TaskDataset(os.path.join(
+            args.data_dir, args.train_data), args, self.label2id)
+        self.valid_data = TaskDataset(os.path.join(
+            args.data_dir, args.valid_data), args, self.label2id)
+        self.test_data = TaskDataset(os.path.join(
+            args.data_dir, args.test_data), args, self.label2id)
+
+    def train_dataloader(self):
+        return DataLoader(self.train_data, shuffle=True, batch_size=self.train_batchsize, pin_memory=False)
+
+    def val_dataloader(self):
+        return DataLoader(self.valid_data, shuffle=False, batch_size=self.valid_batchsize, pin_memory=False)
+
+    def predict_dataloader(self):
+        return DataLoader(self.test_data, shuffle=False, batch_size=self.valid_batchsize, pin_memory=False)
+
+    def load_schema(self, data_path, args):
+        with open(data_path, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+            label_list = [] = []
+            for line in tqdm(lines):
+                data = json.loads(line)
+                labels = data[args.label_name] if args.label_name in data.keys(
+                ) else 0
+                if labels not in label_list:
+                    label_list.append(labels)
+
+        label2id, id2label = {}, {}
+        for i, k in enumerate(label_list):
+            label2id[k] = i
+            id2label[i] = k
+        return label2id, id2label
+
+
+class LitAutoEncoder(pl.LightningModule):
+
+    @staticmethod
+    def add_model_specific_args(parent_args):
+        parser = parent_args.add_argument_group('BaseModel')
+
+        parser.add_argument('--learning_rate', default=1e-5, type=float)
+        parser.add_argument('--weight_decay', default=0.1, type=float)
+        parser.add_argument('--warmup', default=0.01, type=float)
+        parser.add_argument('--num_labels', default=2, type=int)
+
+        return parent_args
+
+    def __init__(self, args, num_data):
+        super().__init__()
+        self.args = args
+        self.num_data = num_data
+        self.model = AutoModelForSequenceClassification.from_pretrained(
+            self.args.pretrained_model_path, num_labels=self.args.num_labels)
+
+    def setup(self, stage) -> None:
+        if stage == 'fit':
+            num_gpus = self.trainer.gpus if self.trainer.gpus is not None else 0
+            self.total_step = int(self.trainer.max_epochs * self.num_data /
+                                  (max(1, num_gpus) * self.trainer.accumulate_grad_batches))
+            print('Total training step:', self.total_step)
+
+    def training_step(self, batch, batch_idx):
+        output = self.model(**batch)
+        acc = self.comput_metrix(output.logits, batch['labels'])
+        self.log('train_loss', output.loss)
+        self.log('train_acc', acc)
+        return output.loss
+
+    def comput_metrix(self, logits, labels):
+        y_pred = torch.argmax(logits, dim=-1)
+        y_pred = y_pred.view(size=(-1,))
+        y_true = labels.view(size=(-1,)).float()
+        corr = torch.eq(y_pred, y_true)
+        acc = torch.sum(corr.float())/labels.size()[0]
+        return acc
+
+    def validation_step(self, batch, batch_idx):
+        output = self.model(**batch)
+        acc = self.comput_metrix(output.logits, batch['labels'])
+        self.log('val_loss', output.loss)
+        self.log('val_acc', acc)
+
+    def predict_step(self, batch, batch_idx):
+        output = self.model(**batch)
+        return output.logits
+
+    def configure_optimizers(self):
+
+        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
+        paras = list(
+            filter(lambda p: p[1].requires_grad, self.named_parameters()))
+        paras = [{
+            'params':
+            [p for n, p in paras if not any(nd in n for nd in no_decay)],
+            'weight_decay': self.args.weight_decay
+        }, {
+            'params': [p for n, p in paras if any(nd in n for nd in no_decay)],
+            'weight_decay': 0.0
+        }]
+        optimizer = torch.optim.AdamW(paras, lr=self.args.learning_rate)
+        scheduler = get_linear_schedule_with_warmup(
+            optimizer, int(self.total_step * self.args.warmup),
+            self.total_step)
+
+        return [{
+            'optimizer': optimizer,
+            'lr_scheduler': {
+                'scheduler': scheduler,
+                'interval': 'step',
+                'frequency': 1
+            }
+        }]
+        
+
+class TaskModelCheckpoint:
+    @staticmethod
+    def add_argparse_args(parent_args):
+        parser = parent_args.add_argument_group('BaseModel')
+
+        parser.add_argument('--monitor', default='train_loss', type=str)
+        parser.add_argument('--mode', default='min', type=str)
+        parser.add_argument('--dirpath', default='./log/', type=str)
+        parser.add_argument(
+            '--filename', default='model-{epoch:02d}-{train_loss:.4f}', type=str)
+
+        parser.add_argument('--save_top_k', default=3, type=float)
+        parser.add_argument('--every_n_train_steps', default=100, type=float)
+        parser.add_argument('--save_weights_only', default=True, type=bool)
+
+        return parent_args
+
+    def __init__(self, args):
+        self.callbacks = ModelCheckpoint(monitor=args.monitor,
+                                         save_top_k=args.save_top_k,
+                                         mode=args.mode,
+                                         every_n_train_steps=args.every_n_train_steps,
+                                         save_weights_only=args.save_weights_only,
+                                         dirpath=args.dirpath,
+                                         filename=args.filename)
+
+
+def save_test(data, args, data_model):
+    with open(args.output_save_path, 'w', encoding='utf-8') as f:
+        idx = 0
+        for i in range(len(data)):
+            batch = data[i]
+            for sample in batch:
+                tmp_result = dict()
+                label_id = np.argmax(sample.numpy())
+                tmp_result['id'] = data_model.test_data.data[idx]['id']
+                tmp_result['label'] = data_model.id2label[label_id]
+                json_data = json.dumps(tmp_result, ensure_ascii=False)
+                f.write(json_data+'\n')
+                idx += 1
+    print('save the result to '+args.output_save_path)
 
 
 def main():
-    parser = HfArgumentParser(TrainingArguments)
-    parser.add_argument("--task_name", type=str, help="test file")
-    parser.add_argument("--train_data_path", type=str, help="train file")
-    parser.add_argument("--dev_data_path", type=str, help="dev file")
-    parser.add_argument("--test_data_path", type=str, help="test file")
-    parser.add_argument("--pretrained_model_path", type=str,
-                        help="pretrained_model_path")
-    parser.add_argument("--max_length", type=int, help="max_length")
+    total_parser = argparse.ArgumentParser("TASK NAME")
+    total_parser.add_argument('--pretrained_model_path', default='', type=str)
+    total_parser.add_argument('--output_save_path',
+                              default='./predict.json', type=str)
 
-    if len(sys.argv) == 2 and sys.argv[1].endswith(".json"):
-        training_args, task_args = parser.parse_json_file(
-            json_file=os.path.abspath(sys.argv[1]))
-    else:
-        training_args, task_args = parser.parse_args_into_dataclasses()
+    # * Args for data preprocessing
+    total_parser = TaskDataModel.add_data_specific_args(total_parser)
+    # * Args for training
+    total_parser = pl.Trainer.add_argparse_args(total_parser)
+    total_parser = TaskModelCheckpoint.add_argparse_args(total_parser)
 
-    label2id, _ = load_schema(task_args.task_name)
-    model = AutoModelForSequenceClassification.from_pretrained(
-        task_args.pretrained_model_path, num_labels=len(label2id))
-    tokenizer = AutoTokenizer.from_pretrained(task_args.pretrained_model_path)
+    # * Args for base model
+    total_parser = LitAutoEncoder.add_model_specific_args(total_parser)
 
-    train_data = load_data(task_args.train_data_path,
-                           task_args.task_name, label2id)
-    dev_data = load_data(task_args.dev_data_path,
-                         task_args.task_name, label2id)
-    train_dataset = taskDataset(
-        train_data, tokenizer=tokenizer, max_length=task_args.max_length)
-    dev_dataset = taskDataset(
-        dev_data, tokenizer=tokenizer, max_length=task_args.max_length)
+    args = total_parser.parse_args()
 
-    trainer = Trainer(
-        model=model,
-        args=training_args,                  
-        train_dataset=train_dataset,        
-        eval_dataset=dev_dataset,        
-        compute_metrics=compute_metrics
-    )
+    checkpoint_callback = TaskModelCheckpoint(args).callbacks
+    trainer = pl.Trainer.from_argparse_args(args,
+                                            callbacks=[checkpoint_callback]
+                                            )
 
-    trainer.train()
+    data_model = TaskDataModel(args)
+    model = LitAutoEncoder(args, len(data_model.train_dataloader()))
+
+    trainer.fit(model, data_model)
+    result = trainer.predict(model, data_model)
+    save_test(result, args, data_model)
 
 
 if __name__ == "__main__":
--- fengshen/scripts/finetune_classification.sh
+++ fengshen/scripts/finetune_classification.sh
@@ -7,46 +7,63 @@
 #SBATCH --gres=gpu:1 # number of gpus per node
 #SBATCH --mail-type=ALL # send email when job begins, ends or failed etc. 
 
-BERT_NAME=rolongformer_base
+BERT_NAME=macbert_base
 ROOT_PATH=cognitive_comp
-TASK=csl
+TASK=tnews
 
-TRAIN_DATA_PATH=/$ROOT_PATH/yangping/data/ChineseCLUE_DATA/${TASK}_public/train.json
-DEV_DATA_PATH=/$ROOT_PATH/yangping/data/ChineseCLUE_DATA/${TASK}_public/dev.json
-TSET_DATA_PATH=/$ROOT_PATH/yangping/data/ChineseCLUE_DATA/${TASK}_public/test.json
-
+DATA_DIR=/$ROOT_PATH/yangping/data/ChineseCLUE_DATA/${TASK}_public/
 
 PRETRAINED_MODEL_PATH=/$ROOT_PATH/yangping/pretrained_model/$BERT_NAME/
 
 CHECKPOINT_PATH=/$ROOT_PATH/yangping/checkpoints/modelevaluation/csl/
-OUTPUT_PATH=/$ROOT_PATH/yangping/nlp/modelevaluation/output/csl_predict.json
+OUTPUT_PATH=/$ROOT_PATH/yangping/nlp/modelevaluation/output/predict.json
 
-LOG_FILE_PATH=/$ROOT_PATH/yangping/nlp/modelevaluation/log/
+
+DATA_ARGS="\
+        --data_dir $DATA_DIR \
+        --train_data train.json \
+        --valid_data dev.json \
+        --test_data test.json \
+        --train_batchsize 16 \
+        --valid_batchsize 64 \
+        --max_length 128 \
+        --texta_name sentence \
+        --label_name label \
+        --id_name id \
+        "
+
+MODEL_ARGS="\
+        --learning_rate 0.00002 \
+        --weight_decay 0.1 \
+        --warmup 0.001 \
+        --num_labels 15 \
+        "
+
+MODEL_CHECKPOINT_ARGS="\
+        --monitor val_acc \
+        --save_top_k 3 \
+        --mode max \
+        --every_n_train_steps 100 \
+        --save_weights_only True \
+        --dirpath $CHECKPOINT_PATH \
+        --filename model-{epoch:02d}-{val_acc:.4f} \
+        "
+TRAINER_ARGS="\
+        --max_epochs 7 \
+        --gpus 1 \
+        --check_val_every_n_epoch 1 \
+        --val_check_interval 100 \
+        --default_root_dir ./log/ \
+        "
+
 
 options=" \
-        --task_name $TASK \
-        --train_data_path $TRAIN_DATA_PATH \
-        --dev_data_path $DEV_DATA_PATH \
-        --test_data_path $TSET_DATA_PATH \
         --pretrained_model_path $PRETRAINED_MODEL_PATH \
-        --max_length 128 \
-        --output_dir $LOG_FILE_PATH \
-        --num_train_epochs 3 \
-        --per_device_train_batch_size 32 \
-        --per_device_eval_batch_size 128 \
-        --learning_rate 0.00002 \
-        --warmup_steps 1000 \
-        --weight_decay 0.01 \
-        --max_grad_norm 0.1 \
-        --logging_dir $LOG_FILE_PATH \
-        --logging_steps 10 \
-        --do_eval true \
-        --evaluation_strategy steps \
-        --save_steps 100 \
-        --eval_steps 100 \
-        --save_total_limit 3 \
-        --load_best_model_at_end true \
-        --metric_for_best_model eval_accuracy \
+        --output_save_path $OUTPUT_PATH \
+        $DATA_ARGS \
+        $MODEL_ARGS \
+        $MODEL_CHECKPOINT_ARGS \
+        $TRAINER_ARGS \
         "
 
 DOCKER_PATH=/$ROOT_PATH/yangping/containers/pytorch21_06_py3_docker_image.sif
