5762a43a2f90ec73ea8273c86c170a8aa4920c9c
ping yang <yangping@idea.edu.cn>
2022-02-09 19:31:54 +0800

    update setup.py

--- setup.py
+++ setup.py
@@ -1,22 +1,22 @@
 from setuptools import setup, find_packages
 
 setup(
-    name = "fengshen",
-    version = "0.0.1",
-    description = "fengshen",
-    long_description = "fengshen",
-    license = "MIT Licence",
-    url = "http://test.com",
-    author = "",
-    author_email = "test@gmail.com",
+    name="fengshen",
+    version="0.0.1",
+    description="fengshen",
+    long_description="fengshen",
+    license="MIT Licence",
+    url="http://test.com",
+    author="",
+    author_email="test@gmail.com",
 
-    packages = find_packages(),
-    include_package_data = True,
-    platforms = "any",
-    install_requires = [],
+    packages=find_packages(),
+    include_package_data=True,
+    platforms="any",
+    install_requires=[],
 
-    scripts = [],
-    entry_points = {
+    scripts=[],
+    entry_points={
         'console_scripts': [
             'test = test.help:main'
         ]
