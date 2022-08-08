f5030304e80ce2f5b7b9ae098d2c593009cd419a
zzasdf <15218404468@163.com>
2022-08-08 18:01:03 +0800

    add wav2vec 2

---
+++ fengshen/data/wav2vec2/wav2vec2_dataset.py
@@ -0,0 +1,307 @@
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+import argparse
+import os
+import sys
+from dataclasses import dataclass
+from typing import Dict, List, Optional, Union, Tuple
+
+import torch
+import logging
+from transformers import (
+    Wav2Vec2FeatureExtractor,
+    Wav2Vec2ForPreTraining,
+)
+from transformers.models.wav2vec2.modeling_wav2vec2 import _compute_mask_indices, _sample_negative_indices
+import numpy as np
+
+logger = logging.getLogger(__name__)
+
+
+def add_data_specific_args(parent_args):
+    parser = parent_args.add_argument_group('Wav2vec2 Dataset')
+    parser.add_argument('--data', type=str)
+    parser.add_argument('--sample_rate', type=float, default=16000)
+    parser.add_argument('--min_sample_size', type=int)
+    parser.add_argument('--max_sample_size', type=int)
+    parser.add_argument('--labels', type=str, nargs='+')
+    parser.add_argument('--enable_padding', type=bool)
+    parser.add_argument('--normalize', type=bool)
+    parser.add_argument('--padding', type=str)
+    parser.add_argument('--datatype', type=str, default="librispeech")
+    return parent_args
+
+
+def uniform_sample_negatives_indices(
+    features_shape: Tuple,
+    num_groups: int,
+    num_vars: int,
+    num_negatives: int,
+    mask_time_indices: Optional[np.ndarray] = None
+):
+    """
+    Sample `num_negatives` vectors from uniform distribution.
+    output: (B, T, num_negatives, num_codevector_groups)
+    (:, :, :, i) range in [i*num_codevectors_per_group, (i-1)*num_codevectors_per_group)
+    """
+    batch_size, sequence_length = features_shape
+    sampled_negative_indices = np.random.randint(
+        0,
+        num_vars,
+        size=(batch_size, sequence_length, num_negatives, num_groups),
+        dtype=np.int32
+    )
+    for i in range(num_groups):
+        sampled_negative_indices[:, :, :, i] += num_vars*i
+
+    return sampled_negative_indices
+
+
+@dataclass
+class DataCollatorForWav2Vec2Pretraining:
+    """
+    Data collator that will dynamically pad the inputs received and prepare masked indices
+    for self-supervised pretraining.
+
+    Args:
+        model (:class:`~transformers.Wav2Vec2ForPreTraining`):
+            The Wav2Vec2 model used for pretraining. The data collator needs to have access
+            to config and ``_get_feat_extract_output_lengths`` function for correct padding.
+        feature_extractor (:class:`~transformers.Wav2Vec2FeatureExtractor`):
+            The processor used for proccessing the data.
+        padding (:obj:`bool`,
+            :obj:`str` or :class:`~transformers.tokenization_utils_base.PaddingStrategy`, `optional`,
+            defaults to :obj:`True`
+        ):
+            Select a strategy to pad the returned sequences (according to the model's padding side and padding index)
+            among:
+            * :obj:`True` or :obj:`'longest'`: Pad to the longest sequence in the batch (or no padding if only a single
+              sequence if provided).
+            * :obj:`'max_length'`: Pad to a maximum length specified with the argument :obj:`max_length` or to the
+              maximum acceptable input length for the model if that argument is not provided.
+            * :obj:`False` or :obj:`'do_not_pad'` (default): No padding (i.e., can output a batch with sequences of
+              different lengths).
+        max_length (:obj:`int`, `optional`):
+            Maximum length of the ``input_values`` of the returned list and optionally padding length (see above).
+        pad_to_multiple_of (:obj:`int`, `optional`):
+            If set will pad the sequence to a multiple of the provided value.
+            This is especially useful to enable the use of Tensor Cores on NVIDIA hardware with compute capability >=
+            7.5 (Volta).
+    """
+
+    model: Wav2Vec2ForPreTraining
+    args: argparse.Namespace
+    feature_extractor: Wav2Vec2FeatureExtractor
+    padding: Union[bool, str] = "longest"
+    pad_to_multiple_of: Optional[int] = None
+
+    def __call__(self, features: List[Dict[str, Union[List[int], torch.Tensor]]]) -> Dict[str, torch.Tensor]:
+        # reformat list to dict and set to pytorch format
+        # print(features)
+
+        for item in features:
+            sample = item["audio"]
+
+            inputs = self.feature_extractor(
+                sample["array"], sampling_rate=sample["sampling_rate"],
+                max_length=self.args.max_sample_size, truncation=True
+            )
+            item["input_values"] = inputs.input_values[0]
+            item["input_length"] = len(inputs.input_values[0])
+
+        features = [{"input_values": item["input_values"]}
+                    for item in features]
+
+        batch = self.feature_extractor.pad(
+            features,
+            padding=self.padding,
+            pad_to_multiple_of=self.pad_to_multiple_of,
+            return_tensors="pt",
+        )
+
+        device = batch["input_values"].device
+        batch_size = batch["input_values"].shape[0]
+
+        mask_indices_seq_length = self.model._get_feat_extract_output_lengths(
+            batch["input_values"].shape[-1])
+        # make sure masked sequence length is a Python scalar
+        mask_indices_seq_length = int(mask_indices_seq_length)
+
+        # make sure that no loss is computed on padded inputs
+        if batch.get("attention_mask") is not None:
+            # compute real output lengths according to convolution formula
+            batch["sub_attention_mask"] = self.model._get_feature_vector_attention_mask(
+                mask_indices_seq_length, batch["attention_mask"]
+            )
+
+        features_shape = (batch_size, mask_indices_seq_length)
+
+        # sample randomly masked indices
+        mask_time_indices = _compute_mask_indices(
+            features_shape,
+            self.model.config.mask_time_prob,
+            self.model.config.mask_time_length,
+            attention_mask=batch.get("sub_attention_mask"),
+        )
+
+        # sample negative indices
+        if self.args.sample_way == "data":
+            sampled_negative_indices = _sample_negative_indices(
+                features_shape,
+                self.model.config.num_negatives,
+                mask_time_indices=mask_time_indices,
+            )
+        else:
+            sampled_negative_indices = uniform_sample_negatives_indices(
+                features_shape=features_shape,
+                num_groups=self.model.config.num_codevector_groups,
+                num_vars=self.model.config.num_codevectors_per_group,
+                num_negatives=self.model.config.num_negatives,
+                mask_time_indices=mask_time_indices,
+            )
+        batch["mask_time_indices"] = torch.tensor(
+            mask_time_indices, dtype=torch.long, device=device)
+        batch["sampled_negative_indices"] = torch.tensor(
+            sampled_negative_indices, dtype=torch.long, device=device)
+
+        return batch
+
+# Copyright (c) Facebook, Inc. and its affiliates.
+#
+# This source code is licensed under the MIT license found in the
+# LICENSE file in the root directory of this source tree.
+
+
+class Wav2vec2Dataset(torch.utils.data.Dataset):
+    def __init__(
+        self,
+        manifest_path,
+        sample_rate,
+        model: Wav2Vec2ForPreTraining,
+        args: argparse.Namespace,
+        feature_extractor: Wav2Vec2FeatureExtractor,
+        padding: Union[bool, str] = "longest",
+        pad_to_multiple_of: Optional[int] = None,
+        max_sample_size=None,
+        min_sample_size=0,
+        shuffle=True,
+        pad=False,
+        normalize=False,
+    ):
+        super().__init__()
+        self.collater_outside = DataCollatorForWav2Vec2Pretraining(
+            model=model,
+            args=args,
+            feature_extractor=feature_extractor,
+            padding=padding,
+            pad_to_multiple_of=pad_to_multiple_of
+        )
+        self.sample_rate = sample_rate
+        self.sizes = []
+        self.max_sample_size = (
+            max_sample_size if max_sample_size is not None else sys.maxsize
+        )
+        self.min_sample_size = min_sample_size
+        self.pad = pad
+        self.shuffle = shuffle
+        self.normalize = normalize
+        skipped = 0
+        self.fnames = []
+        self.texts = []
+        sizes = []
+        self.skipped_indices = set()
+        with open(manifest_path, "r") as f:
+            self.root_dir = f.readline().strip()
+            for i, line in enumerate(f):
+                items = line.strip().split("\t")
+                assert len(items) == 2, "{}".format(line)
+                sz = int(items[1])
+                if min_sample_size is not None and sz < min_sample_size:
+                    skipped += 1
+                    self.skipped_indices.add(i)
+                    continue
+                self.fnames.append(items[0].encode())
+                sizes.append(sz)
+        logger.info(f"loaded {len(self.fnames)}, skipped {skipped} samples")
+
+        self.sizes = np.array(sizes, dtype=np.int64)
+
+        try:
+            import pyarrow
+
+            self.fnames = pyarrow.array(self.fnames)
+        except Exception:
+            logger.debug(
+                "Could not create a pyarrow array. Please install pyarrow for better performance"
+            )
+            pass
+
+    def __getitem__(self, index):
+        """
+            example:
+            {'chapter_id': 141231,
+            'file': '/home/...flac',
+            'audio': {'path': '/home/...flac',
+            'array': array([-0.00048828, -0.00018311, -0.00137329, ...,  0.00079346,
+                    0.00091553,  0.00085449], dtype=float32),
+            'sampling_rate': 16000},
+            'id': '1272-141231-0000',
+            'speaker_id': 1272,
+            }
+            we ignore the 'text' item, which is different from the huggingface librispeech dataset
+        """
+
+        import soundfile as sf
+        fn = self.fnames[index]
+        fn = fn if isinstance(self.fnames, list) else fn.as_py()
+        fn = fn.decode()
+
+        path_or_fp = os.path.join(self.root_dir, fn)
+
+        if path_or_fp.split(".")[-1] == "npy":
+            wav = np.load(path_or_fp)
+            curr_sample_rate = int((path_or_fp.split("_")[-1]).split(".")[0])
+            fn_split = os.path.splitext(os.path.basename(fn))[0].split('-')
+            result = {
+                'file': path_or_fp,
+                'audio': {
+                    'path': path_or_fp,
+                    'array': wav,
+                    'sampling_rate': curr_sample_rate
+                },
+                'id': fn,
+            }
+        else:
+            wav, curr_sample_rate = sf.read(path_or_fp, dtype="float32")
+        # feats = torch.from_numpy(wav).float()
+            fn_split = os.path.splitext(os.path.basename(fn))[0].split('-')
+            result = {
+                'chapter_id': int(fn_split[1]),
+                'file': path_or_fp,
+                'audio': {
+                    'path': path_or_fp,
+                    'array': wav,
+                    'sampling_rate': curr_sample_rate
+                },
+                'id': fn,
+                'speaker_id': int(fn_split[0]),
+            }
+
+        # feats = self.postprocess(feats, curr_sample_rate)
+        return result
+
+    def __len__(self):
+        return len(self.sizes)
+
+    def collater(self, samples):
+        return self.collater_outside(samples)
+
+    def size(self, index):
+        """Return an example's size as a float or tuple. This value is used when
+        filtering a dataset with ``--max-positions``."""
+        if self.pad:
+            return self.sizes[index]
+        return min(self.sizes[index], self.max_sample_size)
---
+++ fengshen/examples/wav2vec2/pretrain_wav2vec2.py
@@ -0,0 +1,265 @@
+from transformers import Wav2Vec2FeatureExtractor
+from transformers import Wav2Vec2Config
+from fengshen.models.wav2vec2.modeling_wav2vec import Wav2Vec2ForPreTraining
+
+import fengshen.data.wav2vec2.wav2vec2_dataset as ctc_datasets
+from fengshen.data.universal_datamodule import UniversalDataModule
+# from transformers.models.hubert.modeling_hubert import _compute_mask_indices
+import argparse
+from pytorch_lightning import (
+    LightningModule,
+    Trainer,
+    loggers,
+)
+from pytorch_lightning.callbacks import LearningRateMonitor
+import torch
+import os
+
+
+class Wav2vec2PretrainDataLoader():
+    def __init__(self, args, model):
+        self.args = args
+        self.load_datasets = {}
+        self.wav2vec2_model = model.model
+        self.feature_extractor = model.feature_extractor
+
+    @property
+    def datasets(self):
+        return self.load_datasets
+
+    def load_dataset(self, split: str, **kwargs):
+        data_path = self.args.data
+        manifest_path = os.path.join(data_path, "{}.tsv".format(split))
+
+        self.datasets[split] = ctc_datasets.Wav2vec2Dataset(
+            manifest_path=manifest_path,
+            sample_rate=self.args.sample_rate,
+            model=self.wav2vec2_model,
+            args=self.args,
+            feature_extractor=self.feature_extractor,
+            max_sample_size=self.args.max_sample_size,
+            min_sample_size=self.args.min_sample_size,
+            pad=self.args.labels is not None or self.args.enable_padding,
+            normalize=self.args.normalize,
+        )
+
+
+def prepare_data(args, model):
+    loader = Wav2vec2PretrainDataLoader(args, model)
+    loader.load_dataset('train')
+    loader.load_dataset('valid')
+    return loader
+
+
+class Wav2vec2Lightning(LightningModule):
+    @staticmethod
+    def add_module_specific_args(parent_parser):
+        parser = parent_parser.add_argument_group('Wav2vec2 Lightning')
+        parser.add_argument(
+            "--audio_column_name",
+            type=str,
+            default="audio",
+            help="Column in the dataset that contains speech file path. Defaults to 'audio'",
+        )
+        parser.add_argument(
+            "--max_gumbel_temperature",
+            type=float,
+            default=2.0,
+            help="Maximum temperature for gumbel softmax.",
+        )
+        parser.add_argument(
+            "--min_gumbel_temperature",
+            type=float,
+            default=0.5,
+            help="Minimum temperature for gumbel softmax.",
+        )
+        parser.add_argument(
+            "--gumbel_temperature_decay", type=float, default=0.999995,
+            help="Decay of gumbel temperature during training."
+        )
+        parser.add_argument(
+            "--pad_to_multiple_of",
+            type=int,
+            default=None,
+            help=(
+                "If set will pad the sequence to a multiple of the provided value. "
+                "This is especially useful to enable the"
+                " use of Tensor Cores on NVIDIA hardware with compute capability"
+                " >= 7.5 (Volta)."
+            ),
+        )
+        parser.add_argument(
+            "--feature_grad_mult",
+            type=float,
+            default=1,
+            help="modified the feature encoder's gradient"
+        )
+        parser.add_argument(
+            "--sample_way",
+            type=str,
+            default="data",
+            help="decide the way to sample negatives"
+        )
+        parser.add_argument(
+            "--num_negatives",
+            type=int,
+            default=100,
+            help="number of negative samples"
+        )
+        return parent_parser
+
+    def __init__(self, args, ** kwargs) -> None:
+        super().__init__()
+        self.save_hyperparameters(args)
+        self.args = args
+        self.feature_extractor = Wav2Vec2FeatureExtractor.from_pretrained(
+            args.model_path)
+        config = Wav2Vec2Config.from_pretrained(args.model_path)
+        config.feature_grad_mult = args.feature_grad_mult
+        config.sample_way = args.sample_way
+        config.num_negatives = args.num_negatives
+        self.config = config
+        self.model = Wav2Vec2ForPreTraining(config=config)
+        self.completed_steps = 0
+        # used to update gumbel temperature
+        self.sub_steps = 0
+        # used for accumulate gradient
+
+    def setup(self, stage) -> None:
+        if stage == 'fit':
+            train_loader = self.trainer._data_connector._train_dataloader_source.dataloader()
+
+            # Calculate total steps
+            if self.trainer.max_epochs > 0:
+                world_size = self.trainer.world_size
+                tb_size = self.hparams.train_batchsize * max(1, world_size)
+                ab_size = self.trainer.accumulate_grad_batches
+                self.total_steps = (len(train_loader.dataset) *
+                                    self.trainer.max_epochs // tb_size) // ab_size
+            else:
+                self.total_steps = self.trainer.max_steps // self.trainer.accumulate_grad_batches
+
+            print('Total steps: {}' .format(self.total_steps))
+
+    def configure_optimizers(self):
+        from fengshen.models.model_utils import configure_optimizers
+        return configure_optimizers(self)
+
+    def forward(self, **batch):
+        batch.pop("sub_attention_mask", None)
+        output = self.model(
+            **batch
+        )
+        return output
+
+    def multiply_grads(self, params, c):
+        """Multiplies grads by a constant *c*."""
+        for p in params:
+            if p.grad is not None:
+                if torch.is_tensor(c):
+                    c = c.to(p.grad.device)
+                p.grad.data.mul_(c)
+
+    def training_step(self, batch, batch_idx):
+        output = self(**batch)
+        num_losses = batch["mask_time_indices"].sum()
+        if self.trainer.accumulate_grad_batches and (self.sub_steps % self.trainer.accumulate_grad_batches == 0):
+            gumbel_temperature = max(
+                self.args.max_gumbel_temperature *
+                self.args.gumbel_temperature_decay**self.completed_steps,
+                self.args.min_gumbel_temperature,
+            )
+            if hasattr(self.model, "module"):
+                self.model.module.set_gumbel_temperature(gumbel_temperature)
+            else:
+                self.model.set_gumbel_temperature(gumbel_temperature)
+            self.completed_steps += 1
+
+        self.sub_steps += 1
+        logs = {
+            "train_loss": output.loss/num_losses,
+            'contrast_loss': output.contrastive_loss/num_losses,
+            'div_loss': output.diversity_loss/num_losses
+        }
+        self.log("train", logs)
+        if self.trainer.accumulate_grad_batches:
+            return {
+                "loss": output.loss/num_losses/self.trainer.accumulate_grad_batches,
+            }
+        else:
+            return {
+                "loss": output.loss/num_losses,
+            }
+
+    def validation_step(self, batch, batch_idx):
+        # print([item for item in batch])
+        num_losses = batch["mask_time_indices"].sum()
+        output = self(**batch)
+        logs = {
+            "train_loss": output.loss/num_losses,
+            'contrast_loss': output.contrastive_loss/num_losses,
+            'div_loss': output.diversity_loss/num_losses
+        }
+        self.log("validation", logs)
+        return {
+            "loss": output.loss/num_losses,
+        }
+
+    def on_save_checkpoint(self, checkpoint) -> None:
+        # Save the current loop info in the mid of epoch
+        # if you lightning <= 1.6.0  uncomment the line below
+        # checkpoint['loops'] = self.trainer.checkpoint_connector._get_loops_state_dict()
+        checkpoint["sub_steps"] = self.sub_steps
+        checkpoint["completed_steps"] = self.completed_steps
+        if self.trainer.global_rank == 0:
+            self.model.save_pretrained(os.path.join(
+                self.trainer.checkpoint_callback.dirpath,
+                'hf_pretrained_epoch{}_step{}'.format(self.trainer.current_epoch, self.trainer.global_step)))
+
+    def on_load_checkpoint(self, checkpoint) -> None:
+        global_step_offset = checkpoint["global_step"]
+        if 'global_samples' in checkpoint:
+            self.consumed_samples = checkpoint['global_samples']
+        self.trainer.fit_loop.epoch_loop._batches_that_stepped = global_step_offset
+        self.completed_steps = checkpoint["completed_steps"]
+        self.sub_steps = checkpoint["sub_steps"]
+
+
+if __name__ == '__main__':
+    args_parser = argparse.ArgumentParser()
+    from fengshen.utils import UniversalCheckpoint
+    from fengshen.models.model_utils import add_module_args
+    args_parser = add_module_args(args_parser)
+    args_parser = ctc_datasets.add_data_specific_args(args_parser)
+    args_parser = UniversalDataModule.add_data_specific_args(args_parser)
+    args_parser = Trainer.add_argparse_args(args_parser)
+    args_parser = Wav2vec2Lightning.add_module_specific_args(args_parser)
+    args_parser = UniversalCheckpoint.add_argparse_args(args_parser)
+    args_parser.add_argument('--ckpt_path', type=str, )
+    args = args_parser.parse_args()
+
+    module = Wav2vec2Lightning(args)
+    data_loader = prepare_data(args, module)
+    data_module = UniversalDataModule(
+        args=args, datasets=data_loader.datasets, tokenizer=None, collate_fn=None)
+
+    data_module.datasets = data_loader.datasets
+
+    lr_monitor = LearningRateMonitor(logging_interval='step')
+    logger = loggers.TensorBoardLogger(save_dir=os.path.join(
+        args.default_root_dir, 'logs/'),
+        name=os.path.basename(os.path.dirname(args.model_path)))
+    checkpoint_callback = UniversalCheckpoint(args).callbacks
+
+    if args.ckpt_path is not None and \
+            not os.path.exists(args.ckpt_path):
+        print('--------warning no checkpoint found--------, remove args')
+        args.ckpt_path = None
+
+    trainer = Trainer.from_argparse_args(args,
+                                         logger=logger,
+                                         callbacks=[
+                                             lr_monitor,
+                                             checkpoint_callback])
+
+    trainer.fit(module, data_module, ckpt_path=args.ckpt_path)
