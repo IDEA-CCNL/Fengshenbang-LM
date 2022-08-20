24f944f3a89bfa5cb192476e45432ce623dc0099
GAOXinyu <gaoxinyu@idea.edu.cn>
2022-08-20 11:26:41 +0000

    TEMP: mod pretrain erlangshen

---
+++ fengshen/data/data_utils/mask_utils.py
@@ -0,0 +1,287 @@
+import collections
+
+import numpy as np
+
+MaskedLmInstance = collections.namedtuple("MaskedLmInstance",
+                                          ["index", "label"])
+
+
+def is_start_piece(piece):
+    """Check if the current word piece is the starting piece (BERT)."""
+    # When a word has been split into
+    # WordPieces, the first token does not have any marker and any subsequence
+    # tokens are prefixed with ##. So whenever we see the ## token, we
+    # append it to the previous set of word indexes.
+    return not piece.startswith("##")
+
+'''
+modified from Megatron-LM
+    Args:
+        tokens: 输入
+        vocab_id_list: 词表token_id_list
+        vocab_id_to_token_dict： token_id到token字典
+        masked_lm_prob：mask概率
+        cls_id、sep_id、mask_id：特殊token
+        max_predictions_per_seq：最大mask个数
+        np_rng：mask随机数
+        max_ngrams：最大词长度
+        do_whole_word_mask：是否做全词掩码
+        favor_longer_ngram：优先用长的词
+        do_permutation：是否打乱
+        geometric_dist：用np_rng.geometric做随机
+        masking_style：mask类型
+        zh_tokenizer：WWM的分词器，比如用jieba.lcut做分词之类的
+'''
+
+
+def create_masked_lm_predictions(tokens,
+                                 vocab_id_list, vocab_id_to_token_dict,
+                                 masked_lm_prob,
+                                 cls_id, sep_id, mask_id,
+                                 max_predictions_per_seq,
+                                 np_rng,
+                                 max_ngrams=3,
+                                 do_whole_word_mask=True,
+                                 favor_longer_ngram=False,
+                                 do_permutation=False,
+                                 geometric_dist=False,
+                                 masking_style="bert",
+                                 zh_tokenizer=None):
+    """Creates the predictions for the masked LM objective.
+    Note: Tokens here are vocab ids and not text tokens."""
+
+    cand_indexes = []
+    # Note(mingdachen): We create a list for recording if the piece is
+    # the starting piece of current token, where 1 means true, so that
+    # on-the-fly whole word masking is possible.
+    token_boundary = [0] * len(tokens)
+    # 如果没有指定中文分词器，那就直接按##算
+    if zh_tokenizer == None:
+        for (i, token) in enumerate(tokens):
+            if token == cls_id or token == sep_id:
+                token_boundary[i] = 1
+                continue
+        # Whole Word Masking means that if we mask all of the wordpieces
+        # corresponding to an original word.
+        #
+        # Note that Whole Word Masking does *not* change the training code
+        # at all -- we still predict each WordPiece independently, softmaxed
+        # over the entire vocabulary.
+            if (do_whole_word_mask and len(cand_indexes) >= 1 and
+                    not is_start_piece(vocab_id_to_token_dict[token])):
+                cand_indexes[-1].append(i)
+            else:
+                cand_indexes.append([i])
+                if is_start_piece(vocab_id_to_token_dict[token]):
+                    token_boundary[i] = 1
+    else:
+        # 如果指定了中文分词器，那就先用分词器分词，然后再进行判断
+        # 获取去掉CLS SEP的原始文本
+        raw_tokens = []
+        for t in tokens:
+            if t != cls_id and t != sep_id:
+                raw_tokens.append(t)
+        raw_tokens = [vocab_id_to_token_dict[i] for i in raw_tokens]
+        # 分词然后获取每次字开头的最长词的长度
+        word_list = set(zh_tokenizer(''.join(raw_tokens), HMM=True))
+        word_length_dict = {}
+        for w in word_list:
+            if len(w) < 1:
+                continue
+            if w[0] not in word_length_dict:
+                word_length_dict[w[0]] = len(w)
+            elif word_length_dict[w[0]] < len(w):
+                word_length_dict[w[0]] = len(w)
+        i = 0
+        # 从词表里面检索
+        while i < len(tokens):
+            token_id = tokens[i]
+            token = vocab_id_to_token_dict[token_id]
+            if len(token) == 0 or token_id == cls_id or token_id == sep_id:
+                token_boundary[i] = 1
+                i += 1
+                continue
+            word_max_length = 1
+            if token[0] in word_length_dict:
+                word_max_length = word_length_dict[token[0]]
+            j = 0
+            word = ''
+            word_end = i+1
+            # 兼容以前##的形式，如果后面的词是##开头的，那么直接把后面的拼到前面当作一个词
+            old_style = False
+            while word_end < len(tokens) and vocab_id_to_token_dict[tokens[word_end]].startswith('##'):
+                old_style = True
+                word_end += 1
+            if not old_style:
+                while j < word_max_length and i+j < len(tokens):
+                    cur_token = tokens[i+j]
+                    word += vocab_id_to_token_dict[cur_token]
+                    j += 1
+                    if word in word_list:
+                        word_end = i+j
+            cand_indexes.append([p for p in range(i, word_end)])
+            token_boundary[i] = 1
+            i = word_end
+
+    output_tokens = list(tokens)
+
+    masked_lm_positions = []
+    masked_lm_labels = []
+
+    if masked_lm_prob == 0:
+        return (output_tokens, masked_lm_positions,
+                masked_lm_labels, token_boundary)
+
+    num_to_predict = min(max_predictions_per_seq,
+                         max(1, int(round(len(tokens) * masked_lm_prob))))
+
+    ngrams = np.arange(1, max_ngrams + 1, dtype=np.int64)
+    if not geometric_dist:
+        # Note(mingdachen):
+        # By default, we set the probilities to favor shorter ngram sequences.
+        pvals = 1. / np.arange(1, max_ngrams + 1)
+        pvals /= pvals.sum(keepdims=True)
+        if favor_longer_ngram:
+            pvals = pvals[::-1]
+    # 获取一个ngram的idx，对于每个word，记录他的ngram的word
+    ngram_indexes = []
+    for idx in range(len(cand_indexes)):
+        ngram_index = []
+        for n in ngrams:
+            ngram_index.append(cand_indexes[idx:idx + n])
+        ngram_indexes.append(ngram_index)
+
+    np_rng.shuffle(ngram_indexes)
+
+    (masked_lms, masked_spans) = ([], [])
+    covered_indexes = set()
+    for cand_index_set in ngram_indexes:
+        if len(masked_lms) >= num_to_predict:
+            break
+        if not cand_index_set:
+            continue
+        # Note(mingdachen):
+        # Skip current piece if they are covered in lm masking or previous ngrams.
+        for index_set in cand_index_set[0]:
+            for index in index_set:
+                if index in covered_indexes:
+                    continue
+
+        if not geometric_dist:
+            n = np_rng.choice(ngrams[:len(cand_index_set)],
+                              p=pvals[:len(cand_index_set)] /
+                              pvals[:len(cand_index_set)].sum(keepdims=True))
+        else:
+            # Sampling "n" from the geometric distribution and clipping it to
+            # the max_ngrams. Using p=0.2 default from the SpanBERT paper
+            # https://arxiv.org/pdf/1907.10529.pdf (Sec 3.1)
+            n = min(np_rng.geometric(0.2), max_ngrams)
+
+        index_set = sum(cand_index_set[n - 1], [])
+        n -= 1
+        # Note(mingdachen):
+        # Repeatedly looking for a candidate that does not exceed the
+        # maximum number of predictions by trying shorter ngrams.
+        while len(masked_lms) + len(index_set) > num_to_predict:
+            if n == 0:
+                break
+            index_set = sum(cand_index_set[n - 1], [])
+            n -= 1
+        # If adding a whole-word mask would exceed the maximum number of
+        # predictions, then just skip this candidate.
+        if len(masked_lms) + len(index_set) > num_to_predict:
+            continue
+        is_any_index_covered = False
+        for index in index_set:
+            if index in covered_indexes:
+                is_any_index_covered = True
+                break
+        if is_any_index_covered:
+            continue
+        for index in index_set:
+            covered_indexes.add(index)
+            masked_token = None
+            token_id = tokens[index]
+            if masking_style == "bert":
+                # 80% of the time, replace with [MASK]
+                if np_rng.random() < 0.8:
+                    masked_token = mask_id
+                else:
+                    # 10% of the time, keep original
+                    if np_rng.random() < 0.5:
+                        masked_token = tokens[index]
+                    # 10% of the time, replace with random word
+                    else:
+                        masked_token = vocab_id_list[np_rng.randint(0, len(vocab_id_list))]
+            elif masking_style == "t5":
+                masked_token = mask_id
+            else:
+                raise ValueError("invalid value of masking style")
+
+            output_tokens[index] = masked_token
+            masked_lms.append(MaskedLmInstance(index=index, label=token_id))
+
+        masked_spans.append(MaskedLmInstance(
+            index=index_set,
+            label=[tokens[index] for index in index_set]))
+
+    assert len(masked_lms) <= num_to_predict
+    np_rng.shuffle(ngram_indexes)
+
+    select_indexes = set()
+    if do_permutation:
+        for cand_index_set in ngram_indexes:
+            if len(select_indexes) >= num_to_predict:
+                break
+            if not cand_index_set:
+                continue
+            # Note(mingdachen):
+            # Skip current piece if they are covered in lm masking or previous ngrams.
+            for index_set in cand_index_set[0]:
+                for index in index_set:
+                    if index in covered_indexes or index in select_indexes:
+                        continue
+
+            n = np.random.choice(ngrams[:len(cand_index_set)],
+                                 p=pvals[:len(cand_index_set)] /
+                                 pvals[:len(cand_index_set)].sum(keepdims=True))
+            index_set = sum(cand_index_set[n - 1], [])
+            n -= 1
+
+            while len(select_indexes) + len(index_set) > num_to_predict:
+                if n == 0:
+                    break
+                index_set = sum(cand_index_set[n - 1], [])
+                n -= 1
+            # If adding a whole-word mask would exceed the maximum number of
+            # predictions, then just skip this candidate.
+            if len(select_indexes) + len(index_set) > num_to_predict:
+                continue
+            is_any_index_covered = False
+            for index in index_set:
+                if index in covered_indexes or index in select_indexes:
+                    is_any_index_covered = True
+                    break
+            if is_any_index_covered:
+                continue
+            for index in index_set:
+                select_indexes.add(index)
+        assert len(select_indexes) <= num_to_predict
+
+        select_indexes = sorted(select_indexes)
+        permute_indexes = list(select_indexes)
+        np_rng.shuffle(permute_indexes)
+        orig_token = list(output_tokens)
+
+        for src_i, tgt_i in zip(select_indexes, permute_indexes):
+            output_tokens[src_i] = orig_token[tgt_i]
+            masked_lms.append(MaskedLmInstance(index=src_i, label=orig_token[src_i]))
+
+    masked_lms = sorted(masked_lms, key=lambda x: x.index)
+    # Sort the spans by the index of the first span
+    masked_spans = sorted(masked_spans, key=lambda x: x.index[0])
+
+    for p in masked_lms:
+        masked_lm_positions.append(p.index)
+        masked_lm_labels.append(p.label)
+    return (output_tokens, masked_lm_positions, masked_lm_labels, token_boundary, masked_spans)
--- fengshen/examples/pretrain_erlangshen/pretrain_erlangshen.py
+++ fengshen/examples/pretrain_erlangshen/pretrain_erlangshen.py
@@ -1,9 +1,8 @@
+from dataclasses import dataclass
 from transformers import (
-    BertTokenizer,
     MegatronBertForPreTraining,
 )
 from pytorch_lightning import (
-    LightningDataModule,
     LightningModule,
     loggers,
     Trainer,
@@ -20,172 +19,29 @@ import sys
 import torch
 import os
 
-sys.path.append('../../')
+'''
+由input处理成samples，也就是最终模型的输入
+其中主要处理逻辑在__call__里
+包含Mask和Sop任务
+'''
+@dataclass
+class ErLangShenCollator:
+    # 一些预处理操作
+    def setup():
+        
+        return
+    
+    def __call__(self, samples):
+        '''
+        samples: 一个sample长这样{"text": "hello world"}
+        '''
+        
+    
 
 
-class MegatronDataModule(LightningDataModule):
-    @staticmethod
-    def add_data_specific_args(parent_parser):
-        parser = parent_parser.add_argument_group('MegatronDataModule')
-        parser.add_argument('--data_prefix', type=str, default='')
-        parser.add_argument('--samples', type=int, nargs='+')
-        parser.add_argument('--num_workers', default=8, type=int)
-        parser.add_argument('--train_batchsize', default=32, type=int)
-        parser.add_argument('--eval_batchsize', default=32, type=int)
-        parser.add_argument('--test_batchsize', default=32, type=int)
-        return parent_parser
-
-    def __init__(self, tokenizer, args, **kwargs):
-        super().__init__()
-        self.tokenizer = tokenizer
-        self.save_hyperparameters(args)
-
-    def setup(self, stage: Optional[str] = None) -> None:
-        if stage != 'fit' and stage != 'validate':
-            raise NotImplementedError
-        from fengshen.data.megatron_dataloader.dataset_utils import build_train_valid_test_datasets
-        self.train_dataset, self.val_dataset, self.test_dataset = build_train_valid_test_datasets(
-            data_prefix=[self.hparams.data_prefix],
-            data_impl='mmap',
-            splits_string='949,50,1',
-            train_valid_test_num_samples=self.hparams.samples,
-            max_seq_length=512,
-            masked_lm_prob=0.15,
-            short_seq_prob=0.1,
-            seed=1234,
-            tokenizer=self.tokenizer,
-            dataset_type='bert_cn_wwm',
-            binary_head=True,
-            skip_warmup=True,
-        )
-
-    def train_dataloader(self):
-        return DataLoader(
-            self.train_dataset,
-            batch_size=self.hparams.train_batchsize,
-            shuffle=False,
-            num_workers=self.hparams.num_workers,
-        )
-
-    def val_dataloader(self):
-        return DataLoader(
-            self.val_dataset,
-            batch_size=self.hparams.eval_batchsize,
-            shuffle=False,
-            num_workers=self.hparams.num_workers,
-        )
-
-    def test_dataloader(self):
-        return DataLoader(
-            self.test_dataset,
-            batch_size=self.hparams.test_batchsize,
-            shuffle=False,
-            num_workers=self.hparams.num_workers,
-        )
-
-
-class MegatronBertLGTN(LightningModule):
-    @staticmethod
-    def add_module_specific_args(args_parser):
-        parser = args_parser.add_argument_group('MegatronBertLGTN')
-        parser.add_argument('--model_path', type=str, default='')
-        parser.add_argument('--learning_rate', default=1e-5, type=float)
-        parser.add_argument('--weight_decay', default=0.1, type=float)
-        parser.add_argument('--warmup', default=0.01, type=float)
-        return args_parser
-
-    def __init__(self, args):
-        super().__init__()
-        self.save_hyperparameters(args)
-        self.model = MegatronBertForPreTraining.from_pretrained(args.model_path)
-
-    def setup(self, stage) -> None:
-        if stage == 'fit':
-            train_loader = self.trainer._data_connector._train_dataloader_source.dataloader()
-
-            # Calculate total steps
-            tb_size = self.hparams.train_batchsize * max(1, self.trainer.gpus)
-            ab_size = self.trainer.accumulate_grad_batches * float(self.trainer.max_epochs)
-            self.total_steps = (len(train_loader.dataset) // tb_size) // ab_size
-
-    def configure_optimizers(self):
-
-        no_decay = ['bias', 'LayerNorm.bias', 'LayerNorm.weight']
-        paras = list(
-            filter(lambda p: p[1].requires_grad, self.named_parameters()))
-        paras = [{
-            'params':
-            [p for n, p in paras if not any(nd in n for nd in no_decay)],
-            'weight_decay': self.hparams.weight_decay
-        }, {
-            'params': [p for n, p in paras if any(nd in n for nd in no_decay)],
-            'weight_decay': 0.0
-        }]
-        optimizer = torch.optim.AdamW(paras, lr=self.hparams.learning_rate)
-        scheduler = get_linear_schedule_with_warmup(
-            optimizer, int(self.total_steps * self.hparams.warmup),
-            self.total_steps)
-
-        return [{
-            'optimizer': optimizer,
-            'lr_scheduler': {
-                'scheduler': scheduler,
-                'interval': 'step',
-                'frequency': 1
-            }
-        }]
-
-    def training_step(self, batch, batch_idx):
-        output = self.model(**batch)
-        self.log('train_loss', output.loss)
-        return output.loss
-
-    def comput_metrix(self, logits, labels):
-        y_pred = torch.argmax(logits, dim=-1)
-        y_pred = y_pred.view(size=(-1,))
-        y_true = labels.view(size=(-1,)).float()
-        corr = torch.eq(y_pred, y_true)
-        acc = torch.sum(corr.float()) / labels.size()[0]
-        return acc
-
-    def validation_step(self, batch, batch_idx):
-        output = self.model(**batch)
-        acc = self.comput_metrix(output.prediction_logits, batch['labels'])
-        print('val_loss ', output.loss)
-        self.log('val_loss', output.loss)
-        self.log('val_acc', acc)
-
-    def predict_step(self, batch, batch_idx):
-        output = self.model(**batch)
-        return output.prediction_logits
-
-
-class CustomCKPT:
-    @staticmethod
-    def add_argparse_args(parent_args):
-        parser = parent_args.add_argument_group('ckpt call back')
-
-        parser.add_argument('--monitor', default='train_loss', type=str)
-        parser.add_argument('--mode', default='min', type=str)
-        parser.add_argument('--dirpath', default='./ckpt/', type=str)
-        parser.add_argument(
-            '--filename', default='model-{epoch:02d}-{train_loss:.4f}', type=str)
-        parser.add_argument('--save_last', action='store_true', default=True)
-        parser.add_argument('--save_top_k', default=3, type=float)
-        parser.add_argument('--every_n_train_steps', default=100, type=float)
-        parser.add_argument('--save_weights_only', action='store_true', default=False)
-
-        return parent_args
-
-    def __init__(self, args):
-        self.callbacks = ModelCheckpoint(monitor=args.monitor,
-                                         save_top_k=args.save_top_k,
-                                         mode=args.mode,
-                                         every_n_train_steps=args.every_n_train_steps,
-                                         save_weights_only=args.save_weights_only,
-                                         dirpath=args.dirpath,
-                                         filename=args.filename,
-                                         save_last=args.save_last)
+class ErLangShen(LightningModule):
+    
+    
 
 
 if __name__ == '__main__':
