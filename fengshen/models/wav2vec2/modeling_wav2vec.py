8a5da85e48d60ebec032373162d7e5a96cae2eb4
zzasdf <15218404468@163.com>
2022-09-27 20:35:11 +0800

    remove get_logit

--- fengshen/models/wav2vec2/modeling_wav2vec.py
+++ fengshen/models/wav2vec2/modeling_wav2vec.py
@@ -1870,64 +1870,6 @@ class Wav2Vec2ForCTC(Wav2Vec2PreTrainedModel):
             loss=loss, logits=logits, hidden_states=outputs.hidden_states, attentions=outputs.attentions
         )
 
-    def get_logits(
-        self,
-        input_values: Optional[torch.Tensor],
-        attention_mask: Optional[torch.Tensor] = None,
-        output_attentions: Optional[bool] = None,
-        output_hidden_states: Optional[bool] = None,
-        return_dict: Optional[bool] = None,
-        labels: Optional[torch.Tensor] = None,
-        normalize=False
-    ) -> Union[Tuple, CausalLMOutput]:
-        r"""
-        labels (`torch.LongTensor` of shape `(batch_size, target_length)`, *optional*):
-            Labels for connectionist temporal classification. Note that `target_length` has to be smaller or equal to
-            the sequence length of the output logits. Indices are selected in `[-100, 0, ..., config.vocab_size - 1]`.
-            All labels set to `-100` are ignored (masked), the loss is only computed for labels in `[0, ...,
-            config.vocab_size - 1]`.
-        """
-
-        return_dict = return_dict if return_dict is not None else self.config.use_return_dict
-
-        outputs = self.wav2vec2(
-            input_values,
-            attention_mask=attention_mask,
-            output_attentions=output_attentions,
-            output_hidden_states=output_hidden_states,
-            return_dict=return_dict,
-        )
-
-        hidden_states = outputs[0]
-        hidden_states = self.dropout(hidden_states)
-
-        logits = self.lm_head(hidden_states)  # should be T x B x N
-        # Assume the 0 of the last dimension means blank
-
-        if labels.max() >= self.config.vocab_size:
-            raise ValueError(
-                f"Label values must be <= vocab_size: {self.config.vocab_size}")
-
-        # retrieve loss input_lengths from attention_mask
-        attention_mask = (
-            attention_mask if attention_mask is not None else torch.ones_like(
-                input_values, dtype=torch.long)
-        )
-
-        # replace the padding token with inf
-        masking_tensor = torch.ones(
-            self.config.vocab_size, device=logits.device
-        ) * float("-inf")
-        masking_tensor[0] = 0
-        padding = self._get_feature_vector_attention_mask(logits.shape[1], attention_mask, add_adapter=False)
-        logits[1-padding] = masking_tensor
-
-        # ctc_loss doesn't support fp16
-        if normalize:
-            logits = nn.functional.log_softmax(logits, dim=-1, dtype=torch.float32)
-
-        return logits
-
 
 @add_start_docstrings(
     """
