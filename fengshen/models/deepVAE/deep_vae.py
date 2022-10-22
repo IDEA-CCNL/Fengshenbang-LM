26415935ba1d7a28999d46f76c3957ece8e384d7
zhongshen zeng <zengzhongshen@idea.edu.cn>
2022-10-22 23:26:44 +0800

    update: update della from txl

--- fengshen/models/deepVAE/deep_vae.py
+++ fengshen/models/deepVAE/deep_vae.py
@@ -12,7 +12,7 @@
 # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
 # See the License for the specific language governing permissions and
 # limitations under the License.
-""" PyTorch TransfoXLDenoise model. """
+""" PyTorch Della model. """
 
 import torch
 import torch.nn as nn
