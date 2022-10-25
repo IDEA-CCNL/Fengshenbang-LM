50745e4911d8883adcdf0f82d286c35806403382
qi yang <yangqi@idea.edu.cn>
2022-10-25 17:03:45 +0800

    Add: Question Generation

---
+++ fengshen/examples/bart_qg/finetune_bart.sh
@@ -0,0 +1,121 @@
+#!/bin/bash
+#SBATCH --job-name=bart_qg # create a short name for your job
+#SBATCH --nodes=1 # node count
+#SBATCH --ntasks-per-node=8 # number of tasks to run per node
+#SBATCH --cpus-per-task=10 # cpu-cores per task (>1 if multi-threaded tasks)
+#SBATCH --gres=gpu:1 # number of gpus per node
+#SBATCH -o %x-%j.log # output and error log file names (%x for job id)
+set -x -e
+
+MODEL_NAME=IDEA-CCNL/Randeng-BART-139M
+RUN_NAME=bart_v0_test
+ROOT_DIR=../../workspace/log/$RUN_NAME
+
+config_json="$ROOT_DIR/$MODEL_NAME.ds_config.json"
+export MASTER_PORT=$[RANDOM%10000+40000]
+
+MICRO_BATCH_SIZE=32
+
+cat <<EOT > $config_json
+{
+    "train_micro_batch_size_per_gpu": $MICRO_BATCH_SIZE,
+    "gradient_clipping": 1,
+    "zero_optimization": {
+        "stage": 1
+    },
+    "fp16": {
+        "enabled": true,
+        "loss_scale": 0,
+        "loss_scale_window": 1000,
+        "initial_scale_power": 16,
+        "hysteresis": 2,
+        "min_loss_scale": 1
+    },
+    "optimizer": {
+        "params": {
+            "betas": [
+                0.9,
+                0.999
+            ],
+            "eps": 1e-08,
+            "lr": 1e-04,
+            "weight_decay": 1e-2
+        },
+        "type": "Adam"
+    },
+    "scheduler": {
+        "params": {
+            "warmup_max_lr": 1e-04,
+            "warmup_min_lr": 1e-08,
+            "total_num_steps": 100000,
+            "warmup_num_steps" : 1000
+        },
+        "type": "WarmupDecayLR"
+    },
+    "steps_per_print": 1000,
+    "zero_allow_untested_optimizer": false
+}
+EOT
+export PL_DEEPSPEED_CONFIG_PATH=$config_json
+export TORCH_EXTENSIONS_DIR=../../workspace/torch_extensions
+
+DATA_ARGS=" \
+        --train_file train.json \
+        --val_file dev.json \
+        --test_file test.json \
+        --tokenizer_type bart \
+        --num_workers 8 \
+        --dataloader_workers 2 \
+        --train_batchsize $MICRO_BATCH_SIZE \
+        --val_batchsize $MICRO_BATCH_SIZE \
+        --test_batchsize $MICRO_BATCH_SIZE  \
+        --max_seq_lengt 512 \
+        --max_src_length 32 \
+        --max_kno_length 416 \
+        --max_tgt_length 64 \
+        --mask_ans_style anstoken_multispan \
+        "  
+
+MODEL_ARGS="\
+        --model_path $MODEL_NAME \
+        --learning_rate 1e-5 \
+        --weight_decay 0.1 \
+        --warmup 0.0001 \
+        "
+
+MODEL_CHECKPOINT_ARGS="\
+        --monitor val_loss \
+        --save_top_k 3 \
+        --mode min \
+        --save_last \
+        --every_n_train_steps 5000 \
+        --save_ckpt_path $ROOT_DIR/ckpt/ \
+        --load_ckpt_path $ROOT_DIR/ckpt/ \
+        --filename model-{step:02d}-{train_loss:.4f} \
+        "
+
+TRAINER_ARGS="\
+        --gradient_clip_val 1.0 \
+        --max_epochs 1 \
+        --gpus 1 \
+        --num_nodes 1 \
+        --strategy deepspeed_stage_1 \
+        --log_every_n_steps 100 \
+        --val_check_interval 0.5 \
+        --accumulate_grad_batches 1 \
+        --default_root_dir $ROOT_DIR \
+        --tensorboard_dir $ROOT_DIR \
+        --label_smooth 0.1 \
+    "
+
+export options=" \
+        $DATA_ARGS \
+        $MODEL_ARGS \
+        $MODEL_CHECKPOINT_ARGS \
+        $TRAINER_ARGS \
+        "
+# test
+export SCRIPT_PATH=./finetune_bart.py
+
+CUDA_VISIBLE_DEVICES=3 python3 ${SCRIPT_PATH} $options > $ROOT_DIR/test.log
+
---
+++ fengshen/examples/bart_qg/utils.py
@@ -0,0 +1,87 @@
+from __future__ import absolute_import
+from __future__ import division
+from __future__ import print_function
+
+import torch
+import torch.nn.functional as F
+import json
+
+
+class QGDataset(torch.utils.data.Dataset):
+    """Some Information about MyDataset"""
+
+    def __init__(self, test_file, sample_num=0):
+        super(QGDataset, self).__init__()
+        self.test_file = test_file
+        with open(test_file, 'r', encoding='utf8') as f:
+            lines = f.readlines()
+            datas = [json.loads(line) for line in lines]
+        if sample_num == 0:
+            self.samples = datas
+        else:
+            self.samples = datas[0:sample_num]
+
+    def __getitem__(self, index):
+        return self.samples[index]
+
+    def __len__(self):
+        return len(self.samples)
+
+
+def test_dataloader(collate_fn, args):
+    """avoid to inference should modify fsdataset and loaddataset"""
+    dataset = QGDataset(args.pred_file, args.sample_num)
+    return torch.utils.data.DataLoader(
+        dataset,
+        batch_size=args.test_batchsize,
+        shuffle=False,
+        num_workers=args.dataloader_workers,
+        collate_fn=collate_fn,
+        # sampler=DistributedSampler(
+        #    self.datasets[self.hparams.test_datasets_field], shuffle=False),
+        pin_memory=True,
+    )
+
+
+class LabelSmoothingCrossEntropy(torch.nn.Module):
+    def __init__(self, smoothing=0.1):
+        super(LabelSmoothingCrossEntropy, self).__init__()
+        self.smoothing = smoothing
+        self.ignore_index = -100
+
+    def forward(self, x, target):
+        confidence = 1. - self.smoothing
+        logprobs = F.log_softmax(x, dim=-1)
+        targets_ignore = torch.where(target != self.ignore_index, target, 0)
+        nll_loss = -logprobs.gather(dim=-1, index=targets_ignore.unsqueeze(1))
+        nll_loss = nll_loss.squeeze(1)
+        smooth_loss = -logprobs.mean(dim=-1)
+        loss = confidence * nll_loss + self.smoothing * smooth_loss
+        return loss.mean()
+
+
+def truncate_sequence(document: str, max_num_tokens: int, reverse=False):
+    total_length = len(document)
+    if total_length <= max_num_tokens:
+        return document
+    else:
+        if reverse:
+            return document[-1*max_num_tokens:]
+        else:
+            return document[:max_num_tokens]
+
+
+def padding_to_maxlength(ids, max_length, pad_id):
+    cur_len = len(ids)
+    len_diff = max_length - len(ids)
+    return ids + [pad_id] * len_diff, [1] * cur_len + [0] * len_diff
+
+
+def white_space_fix(text):
+    return "".join(text.split(" "))
+
+
+def remove_prompt(text):
+    if ":" in text:
+        return text.split(":")[1]
+    return text
