c4ad096d6e1aa5c1ec4237ee950df65388a10842
GAOXinyu <rizia1996@outlook.com>
2022-10-31 20:01:46 +0800

    FEAT：新增scheduler参数， 支持更精细的warmup配置

--- fengshen/models/model_utils.py
+++ fengshen/models/model_utils.py
@@ -2,15 +2,22 @@ from pytorch_lightning import LightningModule
 
 from pytorch_lightning.strategies import DeepSpeedStrategy
 from deepspeed.ops.adam import DeepSpeedCPUAdam, FusedAdam
-from transformers.optimization import AdamW, get_scheduler
+from transformers.optimization import AdamW, TYPE_TO_SCHEDULER_FUNCTION
+from transformers.trainer_utils import SchedulerType
+from typing import Optional, Union
+from torch.optim import Optimizer
 
 
 def add_module_args(parent_args):
     parser = parent_args.add_argument_group('Basic Module')
     parser.add_argument('--learning_rate', default=5e-5, type=float)
-    parser.add_argument('--weight_decay', default=1e-1, type=float)
+    parser.add_argument('--min_learning_rate', default=1e-7, type=float)
+    parser.add_argument('--lr_decay_steps', default=0, type=int)
+    # lr decay的时候会依赖total_steps，这里设置的是total_steps的比例，比如我只需要前50%步做decay，ratio设置为0.5
+    parser.add_argument('--lr_decay_ratio', default=1.0, type=float)
     parser.add_argument('--warmup_steps', default=0, type=int)
     parser.add_argument('--warmup_ratio', default=0.1, type=float)
+    parser.add_argument('--weight_decay', default=1e-1, type=float)
     parser.add_argument('--adam_beta1', default=0.9, type=float)
     parser.add_argument('--adam_beta2', default=0.999, type=float)
     parser.add_argument('--adam_epsilon', default=1e-8, type=float)
@@ -46,8 +53,11 @@ def configure_optimizers(pl_model: LightningModule):
     # Configure learning rate scheduler.
     warmup_steps = pl_model.hparams.warmup_ratio * \
         pl_model.total_steps if pl_model.hparams.warmup_steps == 0 else pl_model.hparams.warmup_steps
+    total_steps = pl_model.hparams.lr_decay_ratio * \
+        pl_model.total_steps if pl_model.hparams.lr_decay_steps == 0 else pl_model.hparams.lr_decay_steps
     scheduler = get_scheduler(name=pl_model.hparams.scheduler_type, optimizer=optimizer,
-                              num_warmup_steps=warmup_steps, num_training_steps=pl_model.total_steps)
+                              num_warmup_steps=warmup_steps, num_training_steps=total_steps,
+                              lr_end=pl_model.hparams.min_learning_rate)
     scheduler = {"scheduler": scheduler, "interval": "step", "frequency": 1}
     return [optimizer], [scheduler]
 
@@ -64,3 +74,48 @@ def get_total_steps(trainer, hparams):
     else:
         total_steps = trainer.max_steps
     return total_steps
+
+
+def get_scheduler(
+    name: Union[str, SchedulerType],
+    optimizer: Optimizer,
+    num_warmup_steps: Optional[int] = None,
+    num_training_steps: Optional[int] = None,
+    lr_end: Optional[float] = None
+):
+    """
+    Unified API to get any scheduler from its name.
+
+    Args:
+        name (`str` or `SchedulerType`):
+            The name of the scheduler to use.
+        optimizer (`torch.optim.Optimizer`):
+            The optimizer that will be used during training.
+        num_warmup_steps (`int`, *optional*):
+            The number of warmup steps to do. This is not required by all schedulers (hence the argument being
+            optional), the function will raise an error if it's unset and the scheduler type requires it.
+        num_training_steps (`int``, *optional*):
+            The number of training steps to do. This is not required by all schedulers (hence the argument being
+            optional), the function will raise an error if it's unset and the scheduler type requires it.
+    """
+    name = SchedulerType(name)
+    schedule_func = TYPE_TO_SCHEDULER_FUNCTION[name]
+    if name == SchedulerType.CONSTANT:
+        return schedule_func(optimizer)
+
+    # All other schedulers require `num_warmup_steps`
+    if num_warmup_steps is None:
+        raise ValueError(f"{name} requires `num_warmup_steps`, please provide that argument.")
+
+    if name == SchedulerType.CONSTANT_WITH_WARMUP:
+        return schedule_func(optimizer, num_warmup_steps=num_warmup_steps)
+
+    # All other schedulers require `num_training_steps`
+    if num_training_steps is None:
+        raise ValueError(f"{name} requires `num_training_steps`, please provide that argument.")
+
+    if name == SchedulerType.POLYNOMIAL:
+        return schedule_func(optimizer, num_warmup_steps=num_warmup_steps,
+                             num_training_steps=num_training_steps, lr_end=lr_end)
+
+    return schedule_func(optimizer, num_warmup_steps=num_warmup_steps, num_training_steps=num_training_steps)
