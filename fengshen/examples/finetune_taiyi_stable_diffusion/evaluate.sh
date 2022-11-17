0ef0c75ff8c25a0ec5ed758cf670e55cd673e78a
xiayu li <lixiayu@mgmt04-eno1>
2022-11-17 16:13:57 +0800

    delete dir

--- fengshen/examples/finetune_taiyi_stable_diffusion/evaluate.sh
+++ fengshen/examples/finetune_taiyi_stable_diffusion/evaluate.sh
@@ -8,6 +8,6 @@
 #SBATCH -p batch
 #SBATCH --qos=ai4cogsys
 
-export SCRIPT_PATH=/cognitive_comp/lixiayu/work/Fengshenbang-LM/fengshen/examples/finetune_taiyi_stable_diffusion/evaluate_model.py
+export SCRIPT_PATH=./evaluate_model.py
 
-srun /home/lixiayu/anaconda3/envs/stable/bin/python $SCRIPT_PATH
+srun python $SCRIPT_PATH
