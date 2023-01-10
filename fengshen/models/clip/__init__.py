3fcc4c2fc12dcb226bbd42720f0837271114be5a
GAOXinyu <rizia1996@outlook.com>
2023-01-10 15:19:31 +0800

    FEAT: support webui

--- fengshen/models/clip/__init__.py
+++ fengshen/models/clip/__init__.py
@@ -1,4 +1,4 @@
-from .modeling_taiyi_clip import TaiyiCLIPModel
+from .modeling_taiyi_clip import TaiyiCLIPModel, TaiyiCLIPEmbedder
 from .processing_taiyi_clip import TaiyiCLIPProcessor
 
-__all__ = ['TaiyiCLIPModel', 'TaiyiCLIPProcessor']
+__all__ = ['TaiyiCLIPModel', 'TaiyiCLIPProcessor', 'TaiyiCLIPEmbedder']
--- fengshen/models/clip/modeling_taiyi_clip.py
+++ fengshen/models/clip/modeling_taiyi_clip.py
@@ -1,5 +1,6 @@
 import torch
 from torch import nn
+from transformers import BertTokenizer
 from transformers.models.clip.modeling_clip import (
     add_start_docstrings,
     add_start_docstrings_to_model_forward,
@@ -251,3 +252,33 @@ class TaiyiCLIPModel(CLIPPreTrainedModel):
             text_model_output=text_outputs,
             vision_model_output=vision_outputs,
         )
+
+
+# use by webui
+class TaiyiCLIPEmbedder(torch.nn.Module):
+    """Uses the Taiyi CLIP transf ormer encoder for text (from Hugging Face)"""
+
+    def __init__(self, version="IDEA-CCNL/Taiyi-Stable-Diffusion-1B-Chinese-v0.1", device="cuda", max_length=512):
+        super().__init__()
+        self.tokenizer = BertTokenizer.from_pretrained(version, subfolder="tokenizer")
+        self.transformer = BertModel.from_pretrained(version+"/text_encoder")
+        self.device = device
+        self.max_length = max_length
+        self.freeze()
+
+    def freeze(self):
+        self.transformer = self.transformer.eval()
+        for param in self.parameters():
+            param.requires_grad = False
+
+    def forward(self, text):
+        batch_encoding = self.tokenizer(text, truncation=True, max_length=self.max_length, return_length=True,
+                                        return_overflowing_tokens=False, padding="max_length", return_tensors="pt")
+        tokens = batch_encoding["input_ids"].to(self.device)
+        outputs = self.transformer(input_ids=tokens)
+
+        z = outputs.last_hidden_state
+        return z
+
+    def encode(self, text):
+        return self(text)
