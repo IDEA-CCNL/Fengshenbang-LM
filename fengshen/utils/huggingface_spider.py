3652ce47db6a671ea04685720d44543adc3e9382
vaibhavb02 <33981696+vaibhavb02@users.noreply.github.com>
2023-06-21 11:29:06 +0530

    Update huggingface_spider.py

--- fengshen/utils/huggingface_spider.py
+++ fengshen/utils/huggingface_spider.py
@@ -2,15 +2,27 @@ import json
 import requests
 from bs4 import BeautifulSoup
 
-response = requests.get('https://huggingface.co/IDEA-CCNL?sort_models=downloads#models')
-soup = BeautifulSoup(response.content, 'html.parser')
-model_data_node = soup.find_all('div', attrs={"class": "SVELTE_HYDRATER"})[3]
-data = json.loads(model_data_node['data-props'])
-all_downloads = 0
-for item in data['repos']:
-    if 'downloads' not in item:
-        item['downloads'] = 0
-    all_downloads += item['downloads']
-    print('name: {}, author: {}, downloads: {}, likes: {}'.format(
-        item['id'], item['author'], item['downloads'], item['likes']))
-print('total downloads {}'.format(all_downloads))
+
+def get_model_downloads(url):
+    response = requests.get(url)
+    if response.status_code != 200:
+        raise Exception("Failed to get response from URL")
+
+    soup = BeautifulSoup(response.content, 'html.parser')
+    model_data_node = soup.find_all('div', attrs={"class": "SVELTE_HYDRATER"})[3]
+    data = json.loads(model_data_node['data-props'])
+    all_downloads = 0
+    for item in data['repos']:
+        if 'downloads' not in item:
+            item['downloads'] = 0
+        all_downloads += item['downloads']
+    return all_downloads
+
+
+def main():
+    all_downloads = get_model_downloads('https://huggingface.co/IDEA-CCNL?sort_models=downloads#models')
+    print(f'Total downloads: {all_downloads}')
+
+
+if __name__ == '__main__':
+    main()
